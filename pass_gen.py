b9c0fdc99147ae0b8f48f8c101e8a0461f576784
Neev lila <141387755+neevlila@users.noreply.github.com>
2024-09-19 15:36:54 +0530

    Create pass_gen.py

---
+++ pass_gen.py
@@ -0,0 +1,15 @@
+# it will generat a random strong password
+# pip install random
+
+import random
+
+lower = "qwertyuiopasdfghjklzxcvbnm"
+upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+number = "1234567890"
+symbol = "!@#$%^&*+"
+
+all = lower + upper + number + symbol
+length = int(input("Enter the length of password : "))
+
+password = "".join(random.sample(all, length))
+print("password:", password)
