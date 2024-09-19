5455a10687ba210d83716e33ea5e6d8ca2945642
Neev lila <141387755+neevlila@users.noreply.github.com>
2024-09-19 15:34:50 +0530

    Create whatsapp_message.py

---
+++ whatsapp_message.py
@@ -0,0 +1,7 @@
+# it will send message automatically at the time which is given by user and that message which is user want to send other person
+# there's one more condition and it is you need to open your browser inn pc /laptop
+# the first digit is for hour and second for minute and it's worked on 24 hours duration
+# First Install pip pywhatkit
+
+import pywhatkit
+pywhatkit.sendwhatmsg('# enter number with country code ', 'Hey ', 8, 47)
