bb06460e588fb5d749d4bd9c131329cd6756394f
Neev lila <141387755+neevlila@users.noreply.github.com>
2025-10-30 14:16:32 +0530

    Refactor internet speed checker script

--- internet-speed-checker.py
+++ internet-speed-checker.py
@@ -1,11 +1,11 @@
-import speedtest
+# pip install speedtest-cli
 
+import speedtest
 
 def bytes_to_mb(bytes):
     KB = 1024
     MB = KB * 1024
     return int(bytes / MB)
-
 
 st = speedtest.Speedtest()
 
